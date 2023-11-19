caf9b2fab19577630bf2eb82f7ca24a64d814343
Tae Hyeon Lee <harry7141@gmail.com>
2023-11-19 17:35:49 +0900

    aws 세팅

--- src/main/java/com/mwom/moyeora/test/controller/TestController.java
+++ src/main/java/com/mwom/moyeora/test/controller/TestController.java
@@ -1,21 +1,40 @@
 package com.mwom.moyeora.test.controller;
 
+import com.mwom.moyeora.test.entity.TestEntity;
+import com.mwom.moyeora.test.repository.TestRepository;
 import com.mwom.moyeora.test.service.TestService;
 import com.mwom.moyeora.test.vo.TestVo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class TestController {
     @Autowired
     private TestService testService;
 
+    private final TestRepository testRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+//    @GetMapping("/selectAllList")
+//    public List<TestVo> selectAllList(){
+//        return testService.selectAllList();
+//    }
+
     @GetMapping("/selectAllList")
-    public List<TestVo> selectAllList(){
-        return testService.selectAllList();
+    public List<TestEntity> selectAllList(){
+        return testRepository.selectAllList();
     }
+
 
     @ResponseBody
     @PostMapping("/insertInfo")
@@ -27,6 +46,7 @@ public class TestController {
     public String hello(){
         String str = "hello world!";
         return str;
+
     }
 
 
