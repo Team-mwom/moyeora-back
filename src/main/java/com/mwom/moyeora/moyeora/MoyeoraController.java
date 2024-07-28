c1905a0fc52ad294f8c64e59f11dda224c0628c9
Yongmini <dev.yongmini@gmail.com>
2024-07-28 17:36:22 +0900

    error

--- src/main/java/com/mwom/moyeora/moyeora/MoyeoraController.java
+++ src/main/java/com/mwom/moyeora/moyeora/MoyeoraController.java
@@ -1,11 +1,26 @@
 package com.mwom.moyeora.moyeora;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @ResponseBody
+@RequiredArgsConstructor
 @RequestMapping("/api/moyeora")
 public class MoyeoraController {
+
+    @Autowired
+    private MoyeoraService moyeoraService;
+
+    @PostMapping("/create-moyeora")
+    public int insertMoyeoraInfo(@RequestBody MoyeoraMainDto moyeoraMainDto) {
+        int result = -1;
+
+        result += moyeoraService.insertMoyeoraInfo(moyeoraMainDto);
+
+        return result;
+    }
 }
--- src/main/java/com/mwom/moyeora/moyeora/MoyeoraRepository.java
+++ src/main/java/com/mwom/moyeora/moyeora/MoyeoraRepository.java
@@ -1,6 +1,7 @@
 package com.mwom.moyeora.moyeora;
 
 import com.mwom.moyeora.domain.entity.Moyeora;
+import com.mwom.moyeora.domain.entity.MoyeoraInfo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MoyeoraRepository extends JpaRepository<Moyeora, Long> {
+
+    Moyeora insertMoyeora(Moyeora moyeora);
+
+    MoyeoraInfo insertMoyeoraInfo(MoyeoraInfo moyeoraInfo);
+
 //    List<Moyeora> findTop4ByOrderByRegDtDesc();
 //
 //    Page<Moyeora> selectMoyeoraList(SearchVo searchVo, Pageable pageable);
