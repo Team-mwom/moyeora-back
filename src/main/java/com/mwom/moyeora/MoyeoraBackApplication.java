97c427ae638615550673fe9dab1c8293f676e812
Tae Hyeon Lee <harry7141@gmail.com>
2024-08-04 17:16:08 +0900

    테스트 제거,프로필 오류 수정

--- src/main/java/com/mwom/moyeora/MoyeoraBackApplication.java
+++ src/main/java/com/mwom/moyeora/MoyeoraBackApplication.java
@@ -2,8 +2,10 @@ package com.mwom.moyeora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+
 public class MoyeoraBackApplication {
 
 	public static void main(String[] args) {
