1d54001ef55ec89bfdbd3413533434f0584d8c17
Yongmini <dev.yongmini@gmail.com>
2024-10-27 17:12:10 +0900

    feat. 카테고리 관련 기능 추가

---
+++ src/main/java/com/mwom/moyeora/category/service/SubCategoryService.java
@@ -0,0 +1,29 @@
+package com.mwom.moyeora.category.service;
+
+import com.mwom.moyeora.database.entity.SubCategoryEntity;
+import com.mwom.moyeora.database.repository.SubCategoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class SubCategoryService {
+
+    @Autowired
+    private final SubCategoryRepository subCategoryRepository;
+
+    /**
+     * 서브 카테고리 전체 검색
+     * SubCategoryService.selectSubCategoryList
+     * @return List<SubCategoryEntity>
+     */
+    public List<SubCategoryEntity> findSubCategoryList(long categorySeq) {
+
+        return subCategoryRepository.findSubCategoryList(categorySeq);
+    }
+}
---
+++ src/main/java/com/mwom/moyeora/database/repository/CategoryRepository.java
@@ -0,0 +1,16 @@
+package com.mwom.moyeora.database.repository;
+
+import com.mwom.moyeora.database.entity.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+
+    /**
+     * 카테고리 전체 검색
+     * CategoryRepository.selectCategoryList
+     * @return List<CategoryEntity>
+     */
+    List<CategoryEntity> findCategoryList();
+}
